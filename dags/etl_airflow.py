0641b548b052177f1c2edf6484c1d75170e3b503
prasadanilmore <prasadanilmore@gmail.com>
2023-10-18 00:48:52 +0200

    Pushing to repo

--- dags/etl_airflow.py
+++ dags/etl_airflow.py
@@ -1,3 +1,4 @@
+# Necessary module imports.
 import time
 from datetime import datetime
 from airflow.models.dag import DAG
@@ -8,28 +9,34 @@ from airflow.hooks.base_hook import BaseHook
 import pandas as pd
 from sqlalchemy import create_engine
 
-#extract tasks
+# Extract phase: Get a list of specific table names from MSSQL.
 @task()
 def get_src_tables():
+    # Connecting to MSSQL using Airflow's MsSqlHook.
     hook = MsSqlHook(mssql_conn_id="sqlserver")
-    sql = """ select  t.name as table_name  
-     from sys.tables t where t.name in ('DimProduct','DimProductSubcategory','DimProductCategory') """
+    # SQL to get specific table names.
+    sql = """
+    select t.name as table_name  
+    from sys.tables t where t.name in ('DimProduct','DimProductSubcategory','DimProductCategory')
+    """
+    # Convert SQL results into a Pandas DataFrame.
     df = hook.get_pandas_df(sql)
-    print(df)
+    # Convert the DataFrame into a dictionary format for easy data manipulation.
     tbl_dict = df.to_dict('dict')
     return tbl_dict
-#
+
+# Extract phase: Load data from MSSQL tables into PostgreSQL tables.
 @task()
 def load_src_data(tbl_dict: dict):
+    # Establish a connection to PostgreSQL.
     conn = BaseHook.get_connection('postgres')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
     all_tbl_name = []
     start_time = time.time()
-    #access the table_name element in dictionaries
+
+    # Loop through tables listed in tbl_dict and load each table's data to PostgreSQL.
     for k, v in tbl_dict['table_name'].items():
-        #print(v)
         all_tbl_name.append(v)
-        rows_imported = 0
         sql = f'select * FROM {v}'
         hook = MsSqlHook(mssql_conn_id="sqlserver")
         df = hook.get_pandas_df(sql)
@@ -40,6 +47,7 @@ def load_src_data(tbl_dict: dict):
     print("Data imported successful")
     return all_tbl_name
 
+# Transform phase for 'DimProduct' table: Transform the extracted data for further processing.
 #Transformation tasks
 @task()
 def transform_srcProduct():
@@ -94,42 +102,41 @@ def transform_srcProductCategory():
     revised.to_sql(f'stg_DimProductCategory', engine, if_exists='replace', index=False)
     return {"table(s) processed ": "Data imported successful"}
 
-#load
+
+# Load phase: Merge data from multiple tables and store in the final product model.
 @task()
 def prdProduct_model():
     conn = BaseHook.get_connection('postgres')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
+    # Reading from staging tables.
     pc = pd.read_sql_query('SELECT * FROM public."stg_DimProductCategory" ', engine)
     p = pd.read_sql_query('SELECT * FROM public."stg_DimProduct" ', engine)
-    p['ProductSubcategoryKey'] = p.ProductSubcategoryKey.astype(float)
-    p['ProductSubcategoryKey'] = p.ProductSubcategoryKey.astype(int)
     ps = pd.read_sql_query('SELECT * FROM public."stg_DimProductSubcategory" ', engine)
-    #join all three
+    
+    # Joining data from the three tables based on keys.
     merged = p.merge(ps, on='ProductSubcategoryKey').merge(pc, on='ProductCategoryKey')
+    # Loading the merged data into the final product model table.
     merged.to_sql(f'prd_DimProductCategory', engine, if_exists='replace', index=False)
     return {"table(s) processed ": "Data imported successful"}
 
-
-# [START how_to_task_group]
+# DAG definition.
 with DAG(dag_id="product_etl_dag",schedule_interval="0 9 * * *", start_date=datetime(2022, 3, 5),catchup=False,  tags=["product_model"]) as dag:
 
+    # TaskGroups group related tasks together for a clear UI representation and logical organization.
     with TaskGroup("extract_dimProudcts_load", tooltip="Extract and load source data") as extract_load_src:
         src_product_tbls = get_src_tables()
         load_dimProducts = load_src_data(src_product_tbls)
-        #define order
         src_product_tbls >> load_dimProducts
 
-    # [START howto_task_group_section_2]
+    # Transform task group.
     with TaskGroup("transform_src_product", tooltip="Transform and stage data") as transform_src_product:
         transform_srcProduct = transform_srcProduct()
         transform_srcProductSubcategory = transform_srcProductSubcategory()
         transform_srcProductCategory = transform_srcProductCategory()
-        #define task order
-        [transform_srcProduct, transform_srcProductSubcategory, transform_srcProductCategory]
 
+    # Load task group.
     with TaskGroup("load_product_model", tooltip="Final Product model") as load_product_model:
         prd_Product_model = prdProduct_model()
-        #define order
-        prd_Product_model
 
+    # Specifying the order of task groups. Extraction -> Transformation -> Loading.
     extract_load_src >> transform_src_product >> load_product_model
